b95c96ca5a5fec4ac4b05aca0cad70477f9fea4d
GuermaziAhmed <ahmedgormazi77@gmail.com>
2024-12-03 14:05:05 +0100

    Update App.js

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
       <NavigationContainer>
       <TabNavigation/>
       </NavigationContainer>
-      <StatusBar style="auto" />
+      <StatusBar />
     </View>
   );
 }
